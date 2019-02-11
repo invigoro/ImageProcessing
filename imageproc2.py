a91aab434b227fe3ec1b3d6041989fc6432ce94f
Timothy Wells <35319035+invigoro@users.noreply.github.com>
2019-02-10 18:36:24 -0700

    We meet again, Cory

--- imageproc2.py
+++ imageproc2.py
@@ -3,23 +3,28 @@ import numpy as np
 
 cap = cv.VideoCapture(0)
 status, img = cap.read()
-height = len(img)
-width = len(img[0])
+
 cv.namedWindow("Video")
 
-blank1 = np.zeros((height, width,3), np.float32)
-blank2 = 255 * np.ones((512, 512, 3), np.float32)
+blank1 = np.float32(img)
+blank2 = np.float32(img)
 
 img1 = blank1
-absDiff = blank1
+absDiff = np.float32(img)
 
 while True:
     status, img = cap.read()
+    blur = cv.GaussianBlur(img,(5,5),0)
+    cv.accumulateWeighted(blur, blank1, .320)
+    res1 = cv.convertScaleAbs(blank1)
+    absDiff = cv.absdiff(img, res1)
 
     cv.imshow("Video", img)
-    cv.imshow("Blank White", blank1)
-    cv.imshow("Blank Black", blank2)
-    cv.imshow("Prev Frame", img1)
+    #cv.imshow("Blur", blur)
+    cv.imshow("Running Avg", res1)
+    cv.imshow("Abs Diff", absDiff)
+    #cv.imshow("Blank White", blank2)
+    #cv.imshow("Prev Frame", img1)
 
     img1 = img
 
