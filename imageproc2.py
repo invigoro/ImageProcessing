a40a2e19851c2fa3770017a1c55452f83e47b397
Timothy Wells <35319035+invigoro@users.noreply.github.com>
2019-02-10 18:09:16 -0700

    Add files via upload

--- imageproc2.py
+++ imageproc2.py
@@ -2,9 +2,12 @@ import cv2 as cv
 import numpy as np
 
 cap = cv.VideoCapture(0)
+status, img = cap.read()
+height = len(img)
+width = len(img[0])
 cv.namedWindow("Video")
 
-blank1 = np.zeros((512,512,3), np.float32)
+blank1 = np.zeros((height, width,3), np.float32)
 blank2 = 255 * np.ones((512, 512, 3), np.float32)
 
 img1 = blank1
@@ -12,13 +15,11 @@ absDiff = blank1
 
 while True:
     status, img = cap.read()
-    cv.absdiff(img, img1, absDiff)
 
     cv.imshow("Video", img)
     cv.imshow("Blank White", blank1)
     cv.imshow("Blank Black", blank2)
     cv.imshow("Prev Frame", img1)
-    cv.imshow("Abs Diff", absDiff)
 
     img1 = img
 
@@ -26,6 +27,5 @@ while True:
     if k == 27:
         break
 
-    
 
 cv.destroyAllWindows()
