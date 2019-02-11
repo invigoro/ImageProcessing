27f057688c55873981c985105406bb733a88ce1a
Timothy Wells <35319035+invigoro@users.noreply.github.com>
2019-02-10 19:04:55 -0700

    uhh

--- imageproc2.py
+++ imageproc2.py
@@ -9,7 +9,7 @@ cv.namedWindow("Video")
 blank1 = np.float32(img)
 blank2 = np.float32(img)
 
-img1 = blank1
+img = np.float32(img)
 absDiff = np.float32(img)
 
 while True:
@@ -18,15 +18,21 @@ while True:
     cv.accumulateWeighted(blur, blank1, .320)
     res1 = cv.convertScaleAbs(blank1)
     absDiff = cv.absdiff(img, res1)
+    grayimg = cv.cvtColor(absDiff, cv.COLOR_BGR2GRAY)
+    _, grayimg = cv.threshold(grayimg, 25, 255, cv.THRESH_BINARY)
+    grayimg = cv.GaussianBlur(grayimg,(5,5),0)
+    #grayimg = cv.cvtColor(absDiff, cv.COLOR_BGR2GRAY)
+    _, grayimg = cv.threshold(grayimg, 220, 255, cv.THRESH_BINARY)
+    img1, contours, hierarchy = cv.findContours(grayimg, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
+
+    cv.drawContours(img, contours, -1, (0,255,0), 3)
     cv.imshow("Video", img)
     #cv.imshow("Blur", blur)
-    cv.imshow("Running Avg", res1)
-    cv.imshow("Abs Diff", absDiff)
+    #cv.imshow("Running Avg", res1)
+    cv.imshow("Abs Diff", grayimg)
     #cv.imshow("Blank White", blank2)
     #cv.imshow("Prev Frame", img1)
-
-    img1 = img
 
     k = cv.waitKey(1)
     if k == 27:
